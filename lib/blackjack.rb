61799028a665a10dffcfbd8aa2570c3259507940
Arslan Ali <ali.arslan319@gmail.com>
2019-03-24 01:42:43 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -24,7 +24,12 @@ def end_game(card_total)
 end
 
 def initial_round
-  # code #initial_round here
+  counter = 0
+  card_one = deal_card
+  card_two = deal_card
+  card_total = card_one + card_two
+  display_card_total(card_total)
+  card_total
 end
 
 def hit?
